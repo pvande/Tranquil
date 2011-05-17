4b5ba7d2cf761246bc4463d5e0da362012fd2a08
Pieter van de Bruggen <pvande@gmail.com>
2011-05-17 00:30:15 -0700

    Refactoring application.js.

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -4,34 +4,30 @@ function buildLayout(layout) {
   var table = document.createElement('table');
   table.id = 'layout';
 
-  layout.forEach(buildLayoutRow(table));
-  document.body.replaceChild(table, document.getElementById('layout'));
-}
-
-function buildLayoutRow(table) {
-  return function(row) {
+  layout.forEach(function(row) {
     if (row.constructor != Array) { row = [row] }
 
     var tr = document.createElement('tr');
-    var divTable = document.createElement('div');
-    var div = document.createElement('div');
-    divTable.className = 'table';
-    div.className = 'row';
+    var td = document.createElement('td');
+    var divRow = document.createElement('div');
+    td.className = 'table';
+    divRow.className = 'row';
 
-    divTable.appendChild(div);
-    tr.appendChild(divTable);
+    tr.appendChild(td);
+    td.appendChild(divRow);
 
-    row.forEach(buildLayoutCell(div, tr));
+    row.forEach(function(cell) {
+      var div = document.createElement('div');
+      div.className = 'cell';
+
+      div.innerHTML = JSON.stringify(cell);
+
+      if ('height' in cell) { tr.style.height = cell.height }
+      divRow.appendChild(div);
+    });
+
     table.appendChild(tr);
-  }
-}
+  });
 
-function buildLayoutCell(row, tr) {
-  return function(cell) {
-    var td = document.createElement('div');
-    td.className = 'cell';
-    td.innerText = JSON.stringify(cell);
-    if ('height' in cell) { tr.style.height = cell.height }
-    row.appendChild(td);
-  }
+  document.body.replaceChild(table, document.getElementById('layout'));
 }
