c96e6d6a4d6ebd21e6dc1ce3cb9c021d2e15aaa0
Pieter van de Bruggen <pvande@gmail.com>
2011-05-21 16:19:04 -0700

    Added more complete support for the chart type.

--- public/javascript/chart.js
+++ public/javascript/chart.js
@@ -1,12 +1,25 @@
 Tranquil['chart'] = function(obj) {
   var div = this;
-  div.innerHTML = Milk.render(
-    '<table><tr>'+
-      '{{#.}}<td><table>' +
-      '<tr><td>' + (obj.info || '{{info}}') + '</td></tr>' +
-      '<tr><td class="bar" style="height: {{percentage}}%; background-color: {{color}}"></td></tr>' +
-    '</table></td>{{/.}}' +
-    '</tr><tfoot><tr>' +
-      '{{#.}}<td>' + (obj.title || '{{title}}') + '</td>{{/.}}' +
-    '</tr></tfoot></table>', (obj.data || []));
+  obj.filter = eval('(' + (obj.filter || 'Object') + ')')
+  div.update = function() {
+    reqwest({
+      url: obj.url,
+      type: 'jsonp',
+      success: function(data) {
+        data = obj.filter(data);
+        var style = 'height: {{percentage}}%; background-color: {{color}}';
+        div.innerHTML = Milk.render(
+          '<table><tr>'+
+            '{{#.}}<td><table>' +
+            '<tr><td>' + (obj.info || '{{info}}') + '</td></tr>' +
+            '<tr><td class="bar" style="' + style + '"></td></tr>' +
+          '</table></td>{{/.}}' +
+          '</tr><tfoot><tr>' +
+            '{{#.}}<td>' + (obj.title || '{{title}}') + '</td>{{/.}}' +
+          '</tr></tfoot></table>', data);
+      },
+    });
+  };
+  div.update();
+  setInterval(div.update, obj.interval || 60000);
 };
