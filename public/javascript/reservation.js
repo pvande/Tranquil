d77f82bbc4208924baf705d72d96e6fe7b917d45
Pieter van de Bruggen <pieter@puppetlabs.com>
2011-06-23 12:44:29 -0700

    Small refactor to the Reservation type.

--- public/javascript/reservation.js
+++ public/javascript/reservation.js
@@ -2,7 +2,8 @@ Tranquil['reservation'] = Tranquil.buildPanel((1).minute(), function(obj, data) 
   var parts = {
     title: '{{title}}',
     reservation: '{{name}}',
-    style: 'top: {{start}}px; height: {{length}}px; background-color: {{color}}',
+    color: '',
+    style: 'top: {{start}}px; height: {{length}}px; background-color: {{>color}}',
   };
 
   data.dID = this.id + '-container';
@@ -11,10 +12,12 @@ Tranquil['reservation'] = Tranquil.buildPanel((1).minute(), function(obj, data) 
   if (obj.template) {
     if (obj.template.title)       { parts.title = obj.template.title }
     if (obj.template.reservation) { parts.title = obj.template.reservation }
+    if (obj.template.color)       { parts.color = obj.template.color }
   }
 
   var t = new Date();
   if (!this.innerHTML) {
+    // Render template
     var tmpl = '<div class="title">{{>title}}</div>';
     tmpl += '<div><div></div><div>';
     tmpl += '<div id="{{dID}}" style="background: -webkit-canvas({{cID}})">';
@@ -24,13 +27,14 @@ Tranquil['reservation'] = Tranquil.buildPanel((1).minute(), function(obj, data) 
     tmpl += '</div></div></div>';
     this.innerHTML = Milk.render(tmpl, data, parts);
 
+    // Draw background
     var canvas = document.getCSSCanvasContext("2d", data.cID, 40, 24 * 60);
     canvas.fillStyle = '#BBB';
     canvas.font = "10px monospace";
     canvas.textBaseline = 'middle';
-    for (var hour = 0; hour < 24; hour++) {
-      var off = hour * 60;
-      canvas.fillText((hour % 12 + 1) + (hour < 12 ? 'a' : 'p') + "m", 15, off);
+    for (var h = 0; h < 24; h++) {
+      var off = h * 60;
+      canvas.fillText((h % 12 + 1) + (h < 12 ? 'a' : 'p') + "m", 15, off);
       canvas.fillRect(0, off, 12, 1);
       canvas.fillRect(0, off + 15, 4, 1);
       canvas.fillRect(0, off + 30, 8, 1);
@@ -38,7 +42,32 @@ Tranquil['reservation'] = Tranquil.buildPanel((1).minute(), function(obj, data) 
     }
   }
 
-  var scrollTime = (t.getHours() - 1) * 60 + (t.getMinutes() + 1) -
-                   this.lastChild.getBoundingClientRect().height *0.25;
+  var scrollTime = (t.getHours() - 1) * 60 + (t.getMinutes() + 1);
+  scrollTime -= this.lastChild.getBoundingClientRect().height *0.25;
   this.lastChild.lastChild.scrollTop = scrollTime;
 });
+
+Tranquil['reservation'].filterGCal = function(data) {
+  var isToday = function(item) {
+    if (item.status == "confirmed" && (item = item.when)) {
+      var startDate = new Date(item[0].start);
+      var today = new Date();
+      return startDate.toDateString() == today.toDateString();
+    }
+    return false;
+  };
+
+  var buildReservation = function(item) {
+    var start = new Date(item.when[0].start);
+    var end   = new Date(item.when[0].end);
+    return {
+      name: item.title,
+      start: (start.getHours() - 1) * 60 + start.getMinutes() - 1,
+      length: (end - start) / (1).minutes(),
+    }
+  };
+
+  data = data.data;
+  data.reservations = data.items.filter(isToday).map(buildReservation);
+  return data;
+}
