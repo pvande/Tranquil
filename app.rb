650fc5111052d5282c03df62f6b4084e6aea54a3
Pieter van de Bruggen <pieter@puppetlabs.com>
2011-06-24 11:03:45 -0700

    Build-in JSONP proxying.

--- app.rb
+++ app.rb
@@ -7,12 +7,19 @@ Dir[__DIR__ + "/vendor/*"].map do |dir|
 end
 
 require 'sinatra'
+require 'open-uri'
+
 get '/' do
-  params['config'] ||= "/config.json"
+  params['config'] ||= "#{request.url.sub(/#{request.path}/, '/')}config.json"
   erb :index
 end
 
-get '/config.json' do
+get '/:json.json' do
   content_type 'text/javascript'
-  (params['callback'] || '') + "(#{File.read(__DIR__ + "/config.json")})"
+  (params['callback'] || '') + "(#{File.read(__DIR__ + "/" + params['json'] + ".json")})"
+end
+
+get '/jsonp' do
+  content_type 'text/javascript'
+  (params['callback'] || '') + "(#{ open(params['url']).read })"
 end
