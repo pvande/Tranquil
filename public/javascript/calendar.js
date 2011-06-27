d891882d77b2c6ff944c6b0a70167bca97f4810c
Pieter van de Bruggen <pvande@gmail.com>
2011-06-26 20:58:34 -0700

    Refactoring the calendar type.

--- public/javascript/calendar.js
+++ public/javascript/calendar.js
@@ -1,14 +1,10 @@
 Tranquil['calendar'] = Tranquil.buildPanel((60).seconds(), function(obj, data) {
-  if (!this.childNodes.length) {
-    this.appendChild(document.createElement('div'));
-    this.appendChild(document.createElement('div'));
-  }
+  obj.template.__proto__ = {
+    title: '{{title}}',
+    event: '{{.}}',
+  };
 
-  var tmpl = obj.template || {};
-  if (!('title' in tmpl)) { tmpl.title = '{{title}}' };
-  if (!('event' in tmpl)) { tmpl.event = '{{.}}' };
-  var events = '{{#events}}<div>' + tmpl.event + '</div>{{/events}}';
-
-  this.firstChild.innerHTML = Milk.render(tmpl.title, data);
-  this.lastChild.innerHTML = Milk.render(events, data);
+  var tmpl = '<div>{{>title}}</div>';
+  tmpl    += '<div>{{#events}}<div>{{>event}}</div>{{/events}}</div>';
+  this.innerHTML = Milk.render(tmpl, data, obj.template);
 });
