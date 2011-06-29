1c3c47a97cd4099cd838614a6ef77376888e704a
Pieter van de Bruggen <pvande@gmail.com>
2011-06-28 20:39:55 -0700

    Cleaning up the ticker type.

--- public/javascript/ticker.js
+++ public/javascript/ticker.js
@@ -1,7 +1,11 @@
-Tranquil['ticker'] = Tranquil.buildPanel((60).seconds(), function(obj, data) {
+Tranquil['ticker'] = Tranquil.buildPanel((1).minute(), function(obj, data) {
   if (!this.childNodes.length) {
     this.appendChild(document.createElement('div'));
   }
+
+  (obj.template = obj.template || {}).__proto__ = {
+    element: '<span>{{.}}</span>',
+  };
 
   var div = this.firstChild;
   var newest = div.lastChild;
@@ -23,7 +27,7 @@ Tranquil['ticker'] = Tranquil.buildPanel((60).seconds(), function(obj, data) {
   // If we have less than ten screen's widths of data buffered, we'll append
   // the data we just queried.
   if (bufferWidth < (10 * div.offsetWidth)) {
-    var tmpl = '{{#.}}' + obj.template + '{{/.}}';
-    div.innerHTML += Milk.render(tmpl, data);
+    var template = '{{#.}}{{>element}}{{/.}}';
+    div.innerHTML += Milk.render(template, data, obj.template);
   }
 });
