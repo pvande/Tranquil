b13c4e8db812b40ec60d995f162ab9fd4c9d341a
Pieter van de Bruggen <pvande@gmail.com>
2011-07-05 16:28:34 -0700

    Cleaning up the Countdown type.

--- public/javascript/countdown.js
+++ public/javascript/countdown.js
@@ -1,3 +1,11 @@
-Tranquil['countdown'] = Tranquil.buildPanel((1).hour(), function(obj, data) {
-  this.innerHTML = Milk.render(obj.template, data);
+Tranquil['countdown'] = Tranquil.buildPanel((3).hour(), function(obj, data) {
+  (obj.template = obj.template || {}).__proto__ = {
+    event: '{{title}}',
+    progressive: 'will happen in',
+    time: '{{daysFromNow}} days',
+    content: '<span class="event">{{>event}}</span>' +
+             ' {{>progressive}} ' +
+             '<span class="time">{{>time}}</span>',
+  };
+  this.innerHTML = Milk.render("{{>content}}", data, obj.template);
 });
