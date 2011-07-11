03c79bb79836aa7d39cfb0d8f76b6dde3193a56c
Pieter van de Bruggen <pvande@gmail.com>
2011-07-10 20:25:09 -0700

    Refactoring the Table type.

--- public/javascript/table.js
+++ public/javascript/table.js
@@ -1,33 +1,13 @@
-Tranquil['table'] = function(obj) {
-  var div = this;
-  div.innerHTML = div.innerHTML || '<table></table>';
+Tranquil['table'] = Tranquil.buildPanel((1).minute(), function(obj, data) {
+  (obj.template = obj.template || {}).__proto__ = {
+    cells: [],
+    _content: '<table>{{>_row}}</table>',
+  };
 
-  var rows = [];
-  var table = document.createElement('table');
-  for (var i = 0; i < obj.data.length; i++) {
-    var cells = [];
-    rows.push(cells);
-    var row = obj.data[i];
-    var tr = document.createElement('tr');
-    for (var j = 0; j < row.length; j++) {
-      var td = document.createElement('td');
-      cells.push(td);
-      td.innerHTML = row[j] || '';
-      td.style.cssText = obj.styles[j];
-      tr.appendChild(td);
-    }
-    table.appendChild(tr);
-  }
-  div.replaceChild(table, div.childNodes[0])
+  obj.template.__proto__._row = Milk.render(
+    '{{=| |=}}|#tr|{{#|&.|}}<tr>|#td|<td>|&.|</td>|/td|</tr>{{/|&.|}}|/tr|',
+    { tr: (obj.keys || []), td: obj.template.cells }
+  );
 
-  if (obj.init) {
-    eval('('+obj.init+')').call(obj, rows);
-  }
-
-  if (obj.update) {
-    var update = eval('('+obj.update+')');
-    div.update = function() { update.call(obj, rows) };
-    div.update();
-    setInterval(div.update, obj.interval || 60000);
-  }
-};
+  this.innerHTML = Milk.render("{{>_content}}", data, obj.template);
+});
