6bc51be6452d7c64090a72a7bf941e9fd3baf62f
Pieter van de Bruggen <pvande@gmail.com>
2012-06-25 16:02:58 -0700

    Make the Static widget consistent with the rest of the types.

--- public/javascript/static.js
+++ public/javascript/static.js
@@ -1,7 +1,14 @@
 Tranquil['static'] = function(obj) {
-  this.innerHTML = obj.content;
+  (obj.template = obj.template || {}).__proto__ = Tranquil['static'].template;
+  this.innerHTML = Milk.render("{{>content}}", obj, obj.template);
 };
+
+Tranquil['static'].javascript = '/javascript/vendor/milk.min.js';
 
 Tranquil['static'].parameters = [
   { name: 'content', type: 'text' },
 ];
+
+Tranquil['static'].template = {
+  content: 'Override <tt>template.content</tt> to hide this message.',
+};
